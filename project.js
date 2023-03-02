08aa9fd7c1cbbfd0bede2d30fc7881d1c3a479e3
ToadIAm <52408111+ToadIAm@users.noreply.github.com>
2023-03-02 20:27:28 +0000

    Add files via upload

---
+++ project.js
@@ -0,0 +1,9 @@
+alert('Hello user');
+
+
+var button_click = document.getElementById('button_click');
+
+button_click.addEventListener('click', function() {
+    button_click.style.color = '#d05774';
+    button_click.style.background = 'grey';
+});
